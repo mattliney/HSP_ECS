771173ae088cae4817e0fd0ac3a0d062c77cb97e
mattliney <M.LINEY-2020@hull.ac.uk>
2023-03-19 17:41:14 +0000

    new collisions

---
+++ HSP_ECS/Components/ComponentCollisionAABB.cs
@@ -0,0 +1,37 @@
+﻿using HSP_ECS.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSP_ECS
+{
+    public class ComponentCollisionAABB : Component
+    {
+        private int mWidth;
+        private int mHeight;
+
+        public ComponentCollisionAABB(int pWidth, int pHeight)
+        {
+            mWidth= pWidth;
+            mHeight= pHeight;
+        }
+
+        public int Width
+        { 
+            get
+            {
+                return mWidth; 
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return mHeight;
+            }
+        }
+    }
+}
---
+++ HSP_ECS/Components/ComponentCollisionPoint.cs
@@ -0,0 +1,26 @@
+﻿using HSP_ECS.Components;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSP_ECS
+{
+    public class ComponentCollisionPoint : Component
+    {
+        private Vector2 mPoint;
+
+        public ComponentCollisionPoint(Vector2 pPoint)
+        {
+            mPoint= pPoint;
+        }
+
+        public Vector2 Point
+        {
+            get { return mPoint; }
+            set { mPoint = value; }
+        }
+    }
+}
