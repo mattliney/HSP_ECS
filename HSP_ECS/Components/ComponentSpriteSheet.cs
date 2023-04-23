ee09f2f1073ad2fb3d73fe68474459cec6865ff8
mattliney <M.LINEY-2020@hull.ac.uk>
2023-04-23 22:58:30 +0100

    component anim. test tomorrow

---
+++ HSP_ECS/Components/ComponentSpriteSheet.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSP_ECS.Components
+{
+    public class ComponentSpriteSheet:Component
+    {
+        private Texture2D mSprite;
+        private Rectangle mSource;
+
+        private int mFrameWidth;
+        private int mFrameHeight;
+        private int mFrameCount;
+        private int mCurrentFrame;
+
+        private float mFrameRate;
+
+        public ComponentSpriteSheet(Texture2D pSprite, int pFrameWidth, int pFrameHeight, int pFrameCount, float pFrameRate)
+        {
+            mName = "ComponentSpriteSheet";
+
+            mSprite = pSprite;
+            mFrameCount= pFrameCount;
+            mFrameWidth= pFrameWidth;
+            mFrameHeight= pFrameHeight;
+            mFrameRate = pFrameRate;
+            mCurrentFrame= 0;
+
+            mSource = new Rectangle(mCurrentFrame * mFrameWidth, 0, mFrameWidth, mFrameHeight);
+        }
+
+        public void UpdateSource()
+        {
+            if(mCurrentFrame >= mFrameCount)
+            {
+                mCurrentFrame= 0;
+            }
+            else
+            {
+                mCurrentFrame++;
+            }
+
+            mSource.X = mCurrentFrame * mFrameWidth;
+        }
+
+        public Texture2D Sprite
+        {
+            get
+            {
+                return mSprite;
+            }
+        }
+
+        public Rectangle Source
+        {
+            get
+            {
+                return mSource;
+            }
+        }
+    }
+}
